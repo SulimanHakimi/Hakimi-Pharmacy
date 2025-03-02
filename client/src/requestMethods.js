0b4a9c6e6560cd0013995b0ed1438b8de7f0d4c0
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2025-03-02 14:22:57 +0430

    update api link

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://hakimi-pharmacy-api.vercel.app";
 
 const getHeaders = () => {
   const token = localStorage.getItem("token");
