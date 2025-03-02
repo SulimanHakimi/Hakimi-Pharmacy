f55c1d380a2cec1f14c79ae8d003ff38809fca9e
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2025-03-02 15:33:09 +0430

    remoove aios

--- admin/src/pages/AdminPrescriptions.jsx
+++ admin/src/pages/AdminPrescriptions.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { deleteRequest, getRequest, putRequest } from "../RequestMethods";
 
 const AdminPrescriptions = () => {
