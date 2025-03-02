36597d8ac9ad2c073e3c95327bd3ced3034b5543
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2025-03-02 13:59:37 +0430

    fix home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import About from './pages/About';
 import Header from './components/header';
